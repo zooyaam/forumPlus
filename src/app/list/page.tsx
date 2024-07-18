ff326fd19d363b214d33584e91b9f64c9fde6c45
zooya <zooyaa@kakao.com>
2024-07-18 18:44:14 +0900

    코드 수정

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,12 +1,15 @@
+import Card from "@/src/components/ui/card";
 import { connectDB } from "@/src/util/db";
 
 export default async function List() {
   let client = await connectDB;
-  const db = client.db("cmnt");
+  const db = client.db("dotory");
   let result = await db.collection("post").find().toArray();
   return (
-    <div>
-      <span>{result[0].title}</span>
+    <div className="flex m-10 flex-col gap-6">
+      {result.map((item) => (
+        <Card title={item.title} />
+      ))}
     </div>
   );
 }
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -4,7 +4,7 @@ interface CardProps {
 
 export default function Card({ title }: CardProps) {
   return (
-    <div className="mx-auto border cursor-pointer py-4 px-4 w-[700px] flex flex-col gap-4 bg-white rounded-xl duration-500 hover:shadow-md">
+    <div className="mx-auto border cursor-pointer py-4 px-4 w-[700px] flex flex-col gap-4 bg-white rounded-xl duration-500 hover:drop-shadow-[0_25px_25px_rgba(0,0,0,0.05)]">
       <h1 className="text-2xl font-semibold">{title}</h1>
       <div className="flex justify-between">
         <p className="text-gray-600">2020년 1월 1일</p>
