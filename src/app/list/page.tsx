d41de1d44951cbc12cb5cba6d3e3b559b1ae8f31
zooya <zooyaa@kakao.com>
2024-07-18 15:06:23 +0900

    폴더 경로 변경

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,10 +1,12 @@
 import { connectDB } from "@/src/util/db";
 
-export default async function Home() {
+export default async function List() {
   let client = await connectDB;
   const db = client.db("cmnt");
   let result = await db.collection("post").find().toArray();
-  console.log(result);
-
-  return <main>{result[0].title}</main>;
+  return (
+    <div>
+      <span>{result[0].title}</span>
+    </div>
+  );
 }
