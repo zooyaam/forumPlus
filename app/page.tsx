b054f65cbab912b8ac38b2fd1583d3ff792b4e24
zooya <zooyaa@kakao.com>
2024-07-18 14:01:08 +0900

    MongoDB 연결

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div>Hello, world!</div>;
+import { connectDB } from "@/src/util/db";
+
+export default async function Home() {
+  let client = await connectDB;
+  const db = client.db("cmnt");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+
+  return <main>{result[0].title}</main>;
 }
