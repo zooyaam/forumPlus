7d99a2179332d8ddb1999418d3a91508ccfa98b7
zooyaam <yjoo2498@naver.com>
2024-07-21 20:34:32 +0900

    delete post ...

--- src/components/ui/card/list-card.tsx
+++ src/components/ui/card/list-card.tsx
@@ -1,11 +1,24 @@
+"use client";
+
+import { connectDB } from "@/src/util/db";
+import { ObjectId } from "mongodb";
 import Link from "next/link";
+import { useEffect } from "react";
 
 interface CardProps {
   title: string;
   id?: string;
 }
 
-export default function ListCard({ title, id }: CardProps) {
+export default async function ListCard({ title, id }: CardProps) {
+  useEffect(() => {});
+  let client = await connectDB;
+  const db = client.db("dotory");
+
+  const handleDelete = (id: string) => {
+    return db.collection("post").deleteOne({ _id: new ObjectId(id) });
+  };
+
   return (
     <div className="mx-auto border relative cursor-pointer py-4 px-4 w-[700px] flex flex-col gap-4 bg-white rounded-xl duration-500 hover:drop-shadow-[0_25px_25px_rgba(0,0,0,0.05)]">
       <h1 className="text-2xl font-semibold">{title}</h1>
@@ -16,10 +29,16 @@ export default function ListCard({ title, id }: CardProps) {
 
       <Link
         href={`/edit/${id}`}
-        className="bg-secondary-300 absolute right-4 font-medium py-0.5 px-2 rounded-lg"
+        className="bg-secondary-300 cursor-pointer absolute right-[90px] font-medium py-0.5 px-2 rounded-lg"
       >
         수정 ✏️
       </Link>
+      <button
+        onClick={handleDelete(id)}
+        className="bg-secondary-400 cursor-pointer absolute right-4 font-medium py-0.5 px-2 rounded-lg"
+      >
+        삭제 🗑️
+      </button>
     </div>
   );
 }
