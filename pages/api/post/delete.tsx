793df1f96a50a4237cd538d6ce19e50f1be3c407
zooya <zooyaa@kakao.com>
2024-07-26 11:04:39 +0900

    validate user action ...

--- pages/api/post/delete.tsx
+++ pages/api/post/delete.tsx
@@ -3,20 +3,24 @@ import { connectDB } from "@/src/util/db";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
 
+// TODO: user validation -> button show-hide
+
 export default async function handler(req: any, res: any) {
   let client = await connectDB;
   const db = client.db("dotory");
 
-  let session = await getServerSession(req, res, authOptions);
-  if (session) {
-    req.body.author = session.user?.email;
-  }
-
   if (req.method === "DELETE") {
-    try {
+    let session = await getServerSession(req, res, authOptions);
+    let result = await db
+      .collection("post")
+      .findOne({ _id: new ObjectId(req.body) });
+
+    // TODO: json -> modal alert message
+    if (result?.author === session?.user?.email) {
       await db.collection("post").deleteOne({ _id: new ObjectId(req.body) });
-    } catch (e) {
-      res.status(500);
+      return res.status(200).json({ message: "삭제되었습니다." });
+    } else {
+      return res.status(500).json({ message: "권한이 없습니다." });
     }
   }
 }
--- pages/api/post/edit.tsx
+++ pages/api/post/edit.tsx
@@ -1,26 +1,38 @@
 import { connectDB } from "@/src/util/db";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+
+// TODO: user validation -> button show-hide
 
 export default async function handler(req: any, res: any) {
-  console.log(req);
   let client = await connectDB;
   const db = client.db("dotory");
 
   if (req.method === "POST") {
-    if (req.body.title === "" || req.body.content === "") {
-      return res.status(400).json({ message: "제목과 내용을 입력해주세요" });
-    }
+    let session = await getServerSession(req, res, authOptions);
+    let result = await db
+      .collection("post")
+      .findOne({ _id: new ObjectId(req.body._id) });
 
-    await db.collection("post").updateOne(
-      { _id: new ObjectId(req.body._id) },
-      {
-        $set: {
-          title: req.body.title,
-          content: req.body.content,
-        },
+    // TODO: json -> modal alert message
+    if (result?.author === session?.user?.email) {
+      if (req.body.title === "" || req.body.content === "") {
+        return res.status(400).json({ message: "제목과 내용을 입력해주세요" });
       }
-    );
 
-    return res.status(200).redirect("/detail/" + req.body._id);
+      await db.collection("post").updateOne(
+        { _id: new ObjectId(req.body._id) },
+        {
+          $set: {
+            title: req.body.title,
+            content: req.body.content,
+          },
+        }
+      );
+      return res.status(200).redirect("/detail/" + req.body._id);
+    } else {
+      return res.status(500).json({ message: "권한이 없습니다." });
+    }
   }
 }
