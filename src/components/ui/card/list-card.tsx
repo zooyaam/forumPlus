0efb3c3293be5ce1ac80227e1e9855501cf97d17
zooya <zooyaa@kakao.com>
2024-07-30 09:48:14 +0900

    modify style

--- src/components/ui/card/list-card.tsx
+++ src/components/ui/card/list-card.tsx
@@ -10,6 +10,7 @@ interface CardProps {
   id?: string;
   name?: string;
   user_profile?: string;
+  post_img?: string;
 }
 
 export default async function ListCard({
@@ -18,6 +19,7 @@ export default async function ListCard({
   author,
   name,
   user_profile,
+  post_img,
 }: CardProps) {
   let session = await getServerSession(authOptions);
 
@@ -25,20 +27,33 @@ export default async function ListCard({
     <div className="relative mx-auto w-full max-w-[700px] transition-all border cursor-pointer py-4 px-4 flex flex-col gap-4 bg-white rounded-xl duration-500 hover:drop-shadow-[0_25px_25px_rgba(0,0,0,0.05)]">
       <Link href={`/detail/${id}`}>
         <div>
-          <div className="flex gap-2 items-center border-b pb-3">
-            <Image
-              src={user_profile ? user_profile : "/black-icon.svg"}
-              alt="작성자 프로필 사진"
-              width={25}
-              height={25}
-              className="rounded-full bg-gray-300"
-            />
-            <p>{name}</p>
+          <div className="flex justify-between border-b pb-3">
+            <div className="flex gap-2 items-center ">
+              <Image
+                src={user_profile ? user_profile : "/black-icon.svg"}
+                alt="작성자 프로필 사진"
+                width={25}
+                height={25}
+                className="rounded-full bg-gray-300"
+              />
+              <p>{name}</p>
+            </div>
+            <p>2020년 1월 2일</p>
           </div>
-          <h1 className="text-2xl py-3 font-semibold block">{title}</h1>
           <div className="flex justify-between">
-            <p className="text-gray-600">2020년 1월 1일</p>
-            <p>조회 1 좋아요 0</p>
+            <div>
+              <h1 className="text-2xl py-3 font-semibold block">{title}</h1>
+              <p className="text-gray-600">조회 1 좋아요 0</p>
+            </div>
+            {post_img && (
+              <Image
+                src={post_img}
+                alt="게시글 이미지"
+                width={30}
+                height={30}
+                className="rounded-xl"
+              />
+            )}
           </div>
         </div>
       </Link>
