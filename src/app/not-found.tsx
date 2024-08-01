f1c09b15a77f37072aaec9264792115870474053
zooya <zooyaa@kakao.com>
2024-08-01 12:06:23 +0900

    markup 404-error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div>
-      <span>Not Found</span>
+    <div className="flex shadow-lg m-5 py-10 gap-10 px-20 justify-between">
+      <div className="flex flex-col gap-10">
+        <p className="text-[#FF5555] font-bold text-6xl">404 Error Page</p>
+        <div>
+          <p className="font-semibold text-4xl pb-3">Page not found</p>
+          <p className="text-xl pb-32">
+            Sorry, the page you are looking for does not exist.
+          </p>
+          <Link
+            href="/"
+            className="bg-[#FF5555] w-fit text-white cursor-pointer hover:opacity-80 duration-300 p-2 rounded-md"
+          >
+            🏠 Go Home
+          </Link>
+        </div>
+      </div>
+
+      <div className="">
+        <Image
+          src="/404-error.jpg"
+          width={500}
+          height={500}
+          alt="에러 이미지"
+        />
+      </div>
     </div>
   );
 }
